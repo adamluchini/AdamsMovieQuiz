bf0a1d7a494deab9e1d2e6046e8a60549900ab68
Adam Luchini <aluchini@gmail.com>
2016-09-30 11:17:28 -0700

    single class addition

--- src/MovieObjectTest/Models/Movie.cs
+++ src/MovieObjectTest/Models/Movie.cs
@@ -15,21 +15,39 @@ namespace MovieObjectTest.Models
         public string Year { get; set; }
         public static Dictionary<string, string> GetMovie()
         {
-            var input = "argo";
-            var client = new RestClient("http://www.omdbapi.com/");
-            var request = new RestRequest("?t=" + input + "&y=&plot=short&r.json", Method.GET);
-            var response = new RestResponse();
+            
+            var input1 = "argo";
+            var client1 = new RestClient("http://www.omdbapi.com/");
+            var request1 = new RestRequest("?t=" + input1 + "&y=&plot=short&r.json", Method.GET);
+            var response1 = new RestResponse();
             Task.Run(async () =>
             {
-                response = await GetResponseContentAsync(client, request) as RestResponse;
+                response1 = await GetResponseContentAsync(client1, request1) as RestResponse;
             }).Wait();
-            MovieClass movieJson = JsonConvert.DeserializeObject<MovieClass>(response.Content);
+            MovieClass movieJson1 = JsonConvert.DeserializeObject<MovieClass>(response1.Content);
             Dictionary<string, string> movieData = new Dictionary<string, string>()
             {
-                {"Title:", movieJson.Title },
-                {"Year:", movieJson.Year }
+                {"Title:", movieJson1.Title },
+                {"Year:", movieJson1.Year }
             };
-           // List<string> list = new List<string>(movieData.Keys);
+            
+
+            var input2 = "braveheart";
+            var client2 = new RestClient("http://www.omdbapi.com/");
+            var request2 = new RestRequest("?t=" + input2 + "&y=&plot=short&r.json", Method.GET);
+            var response2 = new RestResponse();
+            Task.Run(async () =>
+            {
+                response2 = await GetResponseContentAsync(client2, request2) as RestResponse;
+            }).Wait();
+            MovieClass movieJson2 = JsonConvert.DeserializeObject<MovieClass>(response2.Content);
+            Dictionary<string, string> movieData2 = new Dictionary<string, string>()
+            {
+                {"Title:", movieJson2.Title },
+                {"Year:", movieJson2.Year }
+            };
+
+            // List<string> list = new List<string>(movieData.Keys);
             return movieData;
         }
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
