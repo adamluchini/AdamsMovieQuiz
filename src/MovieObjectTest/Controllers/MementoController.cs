af3bb50d0ad79a4e1bb9c8f71b09f7a03576af59
Adam Luchini <aluchini@gmail.com>
2016-10-04 14:19:14 -0700

    take out the give up button for now since it was appending the result

---
+++ src/MovieObjectTest/Controllers/MementoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieObjectTest.Models;
+
+namespace MovieObjectTest.Controllers
+{
+    public class MementoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MementoAnswer()
+        {
+            ViewBag.movieResult = MementoClass.GetMovie();
+            return View();
+        }
+        public IActionResult MementoAnswer2()
+        {
+            ViewBag.movieResult = MementoClass.GetMovie();
+            return View();
+        }
+        public IActionResult MementoAnswer3()
+        {
+            ViewBag.movieResult = MementoClass.GetMovie();
+            return View();
+        }
+        public IActionResult Guess(string userguess)
+        {
+            ViewBag.movieResult = MementoClass.GetMovie();
+            if (userguess.ToLower() == "memento")
+            {
+                return View("Correct");
+            }
+            else
+            {
+                return View("Reveal");
+            }
+        }
+    }
+}
---
+++ src/MovieObjectTest/Models/Memento.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using MovieObjectTest.Controllers;
+using Newtonsoft.Json.Linq;
+
+namespace MovieObjectTest.Models
+{
+    public class MementoClass
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+
+        public static Dictionary<string, string> GetMovie()
+        {
+            /* string[] movieList = { "argo", "braveheart" };
+             var input = "";
+             for (var i = 0; i < movieList.Length; i++)
+             {
+                 input = movieList[0];
+                 input = movieList[1];
+             } */
+            var input = "memento";
+            var clientMemento = new RestClient("http://www.omdbapi.com/");
+            var requestMemento = new RestRequest("?t=" + input + "&y=&plot=short&r.json", Method.GET);
+            var responseMemento = new RestResponse();
+            Task.Run(async () =>
+            {
+                responseMemento = await GetResponseContentAsync(clientMemento, requestMemento) as RestResponse;
+            }).Wait();
+            MementoClass movieJsonMemento = JsonConvert.DeserializeObject<MementoClass>(responseMemento.Content);
+            Dictionary<string, string> movieDataMemento = new Dictionary<string, string>()
+            {
+                {"Title", movieJsonMemento.Title },
+                {"Year", movieJsonMemento.Year },
+                {"Director", movieJsonMemento.Director },
+                {"Genre", movieJsonMemento.Genre },
+                {"Actors", movieJsonMemento.Actors },
+                {"Plot", movieJsonMemento.Plot },
+                {"Poster", movieJsonMemento.Poster }
+            };
+
+            return movieDataMemento;
+
+            // List<string> list = new List<string>(movieData.Keys);
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
