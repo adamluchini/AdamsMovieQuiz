596ffa31ec95eb99938abf163159946ae647c19e
Adam Luchini <aluchini@gmail.com>
2016-10-04 15:47:40 -0700

    added test

---
+++ MovieObjectTest.Tests/ArgoClassTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieObjectTest.Models;
+using Xunit;
+
+namespace MovieObjectTest.Tests
+{
+    public class ArgoClassTest
+    {
+        [Fact]
+        public void GetMovieTitleTest()
+        {
+            var result = movieJsonArgo.Title;
+            Assert.Equal("argo", result);
+        }
+    }
+}
