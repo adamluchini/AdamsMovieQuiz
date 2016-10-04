9c62d9f24f732c71b2369fa34e578c4d771e63bb
Adam Luchini <aluchini@gmail.com>
2016-10-04 16:22:09 -0700

    add inside man

---
+++ src/MovieObjectTest/Controllers/InsideManController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieObjectTest.Models;
+
+namespace MovieObjectTest.Controllers
+{
+    public class Movie3 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult InsideManAnswer()
+        {
+            ViewBag.movieResult = InsideManClass.GetMovie();
+            return View();
+        }
+        public IActionResult InsideManAnswer2()
+        {
+            ViewBag.movieResult = InsideManClass.GetMovie();
+            return View();
+        }
+        public IActionResult InsideManAnswer3()
+        {
+            ViewBag.movieResult = InsideManClass.GetMovie();
+            return View();
+        }
+        public IActionResult Guess(string userguess)
+        {
+            ViewBag.movieResult = InsideManClass.GetMovie();
+            if (userguess == null)
+            {
+                return View("Reveal");
+            }
+            else if (userguess.ToLower() == "inside man")
+            {
+
+                return View("Correct");
+            }
+            else
+            {
+                return View("Reveal");
+            }
+        }
+    }
+}
---
+++ src/MovieObjectTest/Models/InsideMan.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using MovieObjectTest.Controllers;
+using Newtonsoft.Json.Linq;
+
+namespace MovieObjectTest.Models
+{
+    public class InsideManClass
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+
+        public static Dictionary<string, string> GetMovie()
+        {
+            /* string[] movieList = { "argo", "braveheart" };
+             var input = "";
+             for (var i = 0; i < movieList.Length; i++)
+             {
+                 input = movieList[0];
+                 input = movieList[1];
+             } */
+            var input = "inside+man";
+            var clientInsideMan = new RestClient("http://www.omdbapi.com/");
+            var requestInsideMan = new RestRequest("?t=" + input + "&y=&plot=short&r.json", Method.GET);
+            var responseInsideMan = new RestResponse();
+            Task.Run(async () =>
+            {
+                responseInsideMan = await GetResponseContentAsync(clientInsideMan, requestInsideMan) as RestResponse;
+            }).Wait();
+            InsideManClass movieJsonInsideMan = JsonConvert.DeserializeObject<InsideManClass>(responseInsideMan.Content);
+            Dictionary<string, string> movieDataInsideMan = new Dictionary<string, string>()
+            {
+                {"Title", movieJsonInsideMan.Title },
+                {"Year", movieJsonInsideMan.Year },
+                {"Director", movieJsonInsideMan.Director },
+                {"Genre", movieJsonInsideMan.Genre },
+                {"Actors", movieJsonInsideMan.Actors },
+                {"Plot", movieJsonInsideMan.Plot },
+                {"Poster", movieJsonInsideMan.Poster }
+            };
+
+            return movieDataInsideMan;
+
+            // List<string> list = new List<string>(movieData.Keys);
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
