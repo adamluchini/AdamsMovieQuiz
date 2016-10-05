1de83af784741997c519327230fe03e682282cbd
Adam Luchini <aluchini@gmail.com>
2016-10-04 17:50:12 -0700

    drop dead gorgeous

---
+++ src/MovieObjectTest/Controllers/DropDeadController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieObjectTest.Models;
+
+namespace MovieObjectTest.Controllers
+{
+    public class Movie8 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DropDeadAnswer()
+        {
+            ViewBag.movieResult = DropDeadClass.GetMovie();
+            return View();
+        }
+        public IActionResult DropDeadAnswer2()
+        {
+            ViewBag.movieResult = DropDeadClass.GetMovie();
+            return View();
+        }
+        public IActionResult DropDeadAnswer3()
+        {
+            ViewBag.movieResult = DropDeadClass.GetMovie();
+            return View();
+        }
+        public IActionResult Guess(string userguess)
+        {
+            ViewBag.movieResult = DropDeadClass.GetMovie();
+            if (userguess == null)
+            {
+                return View("Reveal");
+            }
+            else if (userguess.ToLower() == "drop dead gorgeous")
+            {
+
+                return View("Correct");
+            }
+            else
+            {
+                return View("Reveal");
+            }
+        }
+    }
+}
---
+++ src/MovieObjectTest/Models/DropDead.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using MovieObjectTest.Controllers;
+using Newtonsoft.Json.Linq;
+
+namespace MovieObjectTest.Models
+{
+    public class DropDeadClass
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+
+        public static Dictionary<string, string> GetMovie()
+        {
+            /* string[] movieList = { "argo", "braveheart" };
+             var input = "";
+             for (var i = 0; i < movieList.Length; i++)
+             {
+                 input = movieList[0];
+                 input = movieList[1];
+             } */
+            var input = "drop+dead+gorgeous";
+            var clientDropDead = new RestClient("http://www.omdbapi.com/");
+            var requestDropDead = new RestRequest("?t=" + input + "&y=&plot=short&r.json", Method.GET);
+            var responseDropDead = new RestResponse();
+            Task.Run(async () =>
+            {
+                responseDropDead = await GetResponseContentAsync(clientDropDead, requestDropDead) as RestResponse;
+            }).Wait();
+            DropDeadClass movieJsonDropDead = JsonConvert.DeserializeObject<DropDeadClass>(responseDropDead.Content);
+            Dictionary<string, string> movieDataDropDead = new Dictionary<string, string>()
+            {
+                {"Title", movieJsonDropDead.Title },
+                {"Year", movieJsonDropDead.Year },
+                {"Director", movieJsonDropDead.Director },
+                {"Genre", movieJsonDropDead.Genre },
+                {"Actors", movieJsonDropDead.Actors },
+                {"Plot", movieJsonDropDead.Plot },
+                {"Poster", movieJsonDropDead.Poster }
+            };
+
+            return movieDataDropDead;
+
+            // List<string> list = new List<string>(movieData.Keys);
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
