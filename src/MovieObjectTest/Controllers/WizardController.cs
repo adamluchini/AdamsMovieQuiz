1d68108561b85f374b16d9673c96e628a46be2d2
Adam Luchini <aluchini@gmail.com>
2016-10-04 17:42:26 -0700

    adds wizard of oz

---
+++ src/MovieObjectTest/Controllers/WizardController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieObjectTest.Models;
+
+namespace MovieObjectTest.Controllers
+{
+    public class Movie7 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WizardAnswer()
+        {
+            ViewBag.movieResult = WizardClass.GetMovie();
+            return View();
+        }
+        public IActionResult WizardAnswer2()
+        {
+            ViewBag.movieResult = WizardClass.GetMovie();
+            return View();
+        }
+        public IActionResult WizardAnswer3()
+        {
+            ViewBag.movieResult = WizardClass.GetMovie();
+            return View();
+        }
+        public IActionResult Guess(string userguess)
+        {
+            ViewBag.movieResult = WizardClass.GetMovie();
+            if (userguess == null)
+            {
+                return View("Reveal");
+            }
+            else if (userguess.ToLower() == "wizard of oz")
+            {
+
+                return View("Correct");
+            }
+            else if (userguess.ToLower() == "the wizard of oz")
+            {
+                return View("Correct");
+            }
+            else
+            {
+                return View("Reveal");
+            }
+        }
+    }
+}
---
+++ src/MovieObjectTest/Models/Wizard.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using MovieObjectTest.Controllers;
+using Newtonsoft.Json.Linq;
+
+namespace MovieObjectTest.Models
+{
+    public class WizardClass
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+
+        public static Dictionary<string, string> GetMovie()
+        {
+            /* string[] movieList = { "wizard+of+oz", "braveheart" };
+             var input = "";
+             for (var i = 0; i < movieList.Length; i++)
+             {
+                 input = movieList[0];
+                 input = movieList[1];
+             } */
+            var input = "wizard+of+oz";
+            var clientWizard = new RestClient("http://www.omdbapi.com/");
+            var requestWizard = new RestRequest("?t=" + input + "&y=&plot=short&r.json", Method.GET);
+            var responseWizard = new RestResponse();
+            Task.Run(async () =>
+            {
+                responseWizard = await GetResponseContentAsync(clientWizard, requestWizard) as RestResponse;
+            }).Wait();
+            WizardClass movieJsonWizard = JsonConvert.DeserializeObject<WizardClass>(responseWizard.Content);
+            Dictionary<string, string> movieDataWizard = new Dictionary<string, string>()
+            {
+                {"Title", movieJsonWizard.Title },
+                {"Year", movieJsonWizard.Year },
+                {"Director", movieJsonWizard.Director },
+                {"Genre", movieJsonWizard.Genre },
+                {"Actors", movieJsonWizard.Actors },
+                {"Plot", movieJsonWizard.Plot },
+                {"Poster", movieJsonWizard.Poster }
+            };
+
+            return movieDataWizard;
+
+            // List<string> list = new List<string>(movieData.Keys);
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
